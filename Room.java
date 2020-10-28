04f0ca0eebdff3890e3d1fd6088bde6c52291720
chris-brown-75 <brownc75@wwu.edu>
2020-10-27 17:32:56 -0700

    commenting

--- Room.java
+++ Room.java
@@ -7,8 +7,9 @@ public class Room{
    private Room goLeft;
    private Room goRight;
    
+   /* Room initializer */
    public Room(){
-      occupants = new ArrayList<>();
+      occupants =  new ArrayList<>(); 
       
       goUp = null; //door going up
       goDown = null; //door going down
@@ -16,11 +17,25 @@ public class Room{
       goRight = null; //door going right
    }
    
+   /* 
+   getOccupants()
+   Returns: ArrayList
+   Precondition: There are player(s) in a room
+   Postcondition: The player(s) are identified and returned
+   This method identifies all the players that are in the room
+   */
    public ArrayList<Player> getOccupants(){
       //return occupants.clone();
       return new ArrayList<Player>(); //temp
    }
    
+   /* 
+   isPlayerHere()
+   Returns: boolean
+   Precondtion: the board needs to keep track of the player's position
+   Postcondtion: the players position is found
+   This method checks where the player's postion is, as in, what room they are in
+   */
    public boolean isPlayerHere(){
       return false; //temp
    }
