0618c07c88be651726677bd8314a9f21c61f2caa
chris-brown-75 <brownc75@wwu.edu>
2020-10-27 17:32:31 -0700

    commenting

--- CastingOffice.java
+++ CastingOffice.java
@@ -2,10 +2,18 @@ public class CastingOffice{
    private int[] prices_money; //rank prices in money
    private int[] prices_credit; //rank prices in credit
    
+   /* Casting office initialization */
    public CastingOffice(){
       //set up the rank and money prices
    }
    
+   /*
+   getRankMoneyPrices()
+   returns: int
+   Precondition: the player wants to know the ranks value in monetary value.
+   Postcondition: the player knows the desired ranks value in monetary value.
+   This method gets the money price of the desired rank.
+   */
    public int[] getRankMoneyPrices(){
       /*
       int[] temp = new int[prices_money.length];
@@ -16,6 +24,13 @@ public class CastingOffice{
       return new int[1]; //temp
    }
    
+   /*
+   getRankCreditPrices()
+   returns: int
+   Precondition: the player wants to know the ranks value in credit value. 
+   Postcondition: the player knows the desired ranks value in credit valiue.
+   This method gets the credit price of the desired rank.
+   */
    public int[] getRankCreditPrices(){
       /*
       int[] temp = new int[prices_credit.length];
