f86b5679f4cddd4bd2108c5832d2f01555593b9c
soneelneumann <neumans@wwu.edu>
2020-11-03 21:54:07 -0800

    Update Moderator

--- Moderator.java
+++ Moderator.java
@@ -11,35 +11,60 @@ public class Moderator{
    /*
       checkMove()
       params:
-         Player p: player moving
+         Room start: room player starts in
          Room dest: room they are moving to
       returns: boolean- true if valid move
-      precond: it is currently player's turn
+      precond: it is currently player's turn, 
+         and they are about to move but haven't yet
       postcond: move is checked for legality
    */
-   public boolean checkMove(Player p, Room dest){
+   public boolean checkMove(Room start, Room dest){
       //
-      return false; //temp
+      if(start.neighbors.contains(dest)){
+         return true;
+      }
+      return false;
    }
    
    /*
       checkRankUp()
+      
       params:
          Player p, player requesting rank
          int rankRequested, rank they wish to purhcase
+         String currencyType, either "c" or "m" for credits or money
       returns: boolean, true if player can purchase, false otherwise
+      
       precond: currently player's turn
       postcond: validity of request is returned
    */
-   public boolean checkRankUp(Player p, int rankRequested){
+   public boolean checkRankUp(Player p, int rankRequested, String currencyType){
       //
-      return false; //temp
+      if(/*p.rank == 6 (p.getRank when it's added)*/|| p.rank >= rankRequested){
+         return false;
+      }
+      /*
+      //switch for different currency types
+      switch(currencyType){
+         case "m":
+            if(player.getMoney() >= cost of the rank in money){
+               return false; 
+            }
+            return true;
+            break;
+         case "c":
+            if(player.gerCredits() >= cost of the rank in credits)
+      }
+      */
+      
    }
    
    /*
       checkAct()
-      params: none
+      params: Player p, who 
       returns: boolean
+      
+      this doesn't need to be here maybr. just use Dice to roll and have Player check maybe
    */
    public boolean checkAct(Player p){
       //might be superfluous????????????????
