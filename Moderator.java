7fc21b7bc53c5338dfbd7c8a0fa3fa0285257361
soneelneumann <neumans@wwu.edu>
2020-11-03 21:41:30 -0800

    slight change to moderator

--- Moderator.java
+++ Moderator.java
@@ -72,7 +72,17 @@ public class Moderator{
       }
    }
    
-   public void dispersePayout(ArrayList<Role> sceneRoles){
+   /*
+      checkGameOver()
+      params: 
+         Scene s, scene we're paying out for
+         Role r, role the player has
+      description: pays player for role as scene ends
+      returns: int, amount to add to player's money
+      precond: scene is being wrapped up
+      postcond: player has been payed
+   */
+   public int dispersePayout(Scene s, Role r){
       //
    }
    
